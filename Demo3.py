bb417ab26b8bd3c7456a01cb00e4f7e647614922
Piyush Moroliya <pmoroliya4@gmail.com>
2026-03-11 13:04:22 +0530

    Add files via upload

---
+++ Demo3.py
@@ -0,0 +1,116 @@
+# a = [1,2,3,4,5,6,7,8,9]
+# a[::2]=10,20,30,40,50,60
+# print(a)
+
+# a = [1,2,3,4,5]
+# print(a[3:0:-1])
+
+# arr = [[1,2,3,4],
+#        [4,5,6,7],
+#        [8,9,10,11],
+#        [12,13,14,15]]
+# for i in range(0, 4):
+#     print(arr[i].pop())
+
+# arr = [1,2,3,4,5,6]
+# for i in range(1,6):
+#     arr[i - 1] = arr[i]
+# for i in range(0, 6):
+#     print(arr[i], end = " ")
+
+# first code 
+
+# name = "piyush moroliya"
+# print(int(3.14))
+# print(int(True))
+# print(int(False))
+# print(int("4"))
+
+# print(bool(0))
+# print(bool(15))
+# print(bool(3.14))
+# print(bool(0.0))
+# print(bool(1+2j))
+# print(bool(0 + 0j))
+# print(bool(-1))
+# print(bool(False))
+# print(bool(True))
+
+# num = 123
+# a = num % 10
+# num = num // 10
+# print(num)
+# b = num % 10
+# c  = num // 10
+# nev = a*100 + b*10 + c*1 
+# print(nev)
+
+
+# num = 1234567
+
+# a = num % 10
+# num = num // 10
+
+# b = num % 10
+# num = num // 10
+
+# c = num % 10
+# num = num // 10
+
+# d = num % 10
+# num = num // 10
+
+# e = num % 10
+# num = num // 10
+
+# f = num % 10
+# num = num // 10
+
+# g = num % 10
+
+# rev = a*1000000 + b*100000 + c*10000 + d*1000 + e*100 + f*10 + g*1
+
+# print(rev)
+
+
+# inch = h*12
+# cm = inch * 2.5
+# print(type(inch))
+# print(type(cm))
+
+# a = {(1,2):1,(2,3):2,(4,5):3}
+# print(a[4,5])
+
+# a = {'a':1,'b':2,'c':3}
+# print(a['a','b'])
+
+# arr = {}
+# arr[1] = 1
+# arr['1'] = 2
+# arr[1] += 1
+# print(arr)
+# sum = 0
+# for k in arr:
+#     sum += arr[k]
+# print(sum)    
+
+# my_dict = {}
+# my_dict [(1,2,4)] = 8
+# my_dict [(4,2,1)] = 10
+# my_dict[(1,2)] = 12
+# sum = 0
+# for k in my_dict:
+#     sum += my_dict[k]
+# print(sum)
+# print(my_dict)
+
+maths = 40
+chemistry = 40
+phy = 60
+print(id(maths))
+print(id(phy))
+
+
+
+
+
